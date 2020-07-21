5cd939bba5815ab7e2f3cf4902039c0591391d33
2dukes <ruipinto02@hotmail.com>
2020-07-21 13:26:55 +0100

    CGRA's Project, final Version.

---
+++ Proj CGRA (a submeter)/proj-t3g3-1/MyInterface.js
@@ -0,0 +1,63 @@
+/**
+* MyInterface
+* @constructor
+*/
+class MyInterface extends CGFinterface {
+    constructor() {
+        super();
+    }
+
+    init(application) {
+        // call CGFinterface init
+        super.init(application);
+        // init GUI. For more information on the methods, check:
+        // http://workshop.chromeexperiments.com/examples/gui
+        this.gui = new dat.GUI();
+        
+        var obj = this;
+
+        //Checkbox element in GUI
+        this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+        this.gui.add(this.scene, 'displayCilinder').name('Display Cilinder');
+        this.gui.add(this.scene, 'displaySphere').name('Display Sphere');
+        this.gui.add(this.scene, 'selectedBackground', this.scene.backgrouds).onChange(this.scene.onSelectedBackgroundChanged.bind(this.scene)).name('Background');
+        this.gui.add(this.scene, 'speedFactor', 0.1, 3).name('Speed Factor');
+        this.gui.add(this.scene, 'scaleFactorVehicle', 0.5, 3).name('Scale Vehicle');
+        this.gui.add(this.scene, 'scaleFactorScene', 0.5, 3).name('Scale Scene');
+        this.gui.add(this.scene, 'displayTerrain').name('Display Terrain');
+        this.gui.add(this.scene, 'displayBillboard').name('Display Billboard');
+
+        this.initKeys();
+
+        return true;
+    }
+
+    initKeys() {
+        // create reference from the scene to the GUI
+        this.scene.gui = this;
+
+        // disable the processKeyboard function
+        this.processKeyboard = function() {};
+
+        // create a named array to store which keys are being pressed
+        this.activeKeys = {};
+    }
+
+    processKeyDown(event) {
+        // called when a key is pressed down
+        // mark it as active in the array
+        this.activeKeys[event.code] = true;
+    };
+
+    processKeyUp(event) {
+        // called when a key is released, mark it as inactive in the array
+        this.activeKeys[event.code] = false;
+    };
+
+    isKeyPressed(keyCode) {
+        // returns true if a key is marked as pressed, false otherwise
+        return this.activeKeys[keyCode] || false;
+    }
+
+    
+}
---
+++ proj/MyCubeQuad.js
@@ -0,0 +1,135 @@
+/**
+ * MyCubeQuad
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyCubeQuad extends CGFobject {
+	constructor(scene) {
+        super(scene);
+        this.quad = new MyQuad(this.scene);
+        this.initMaterials(scene);
+    }
+    initMaterials(scene) {
+            this.skyBg = new CGFappearance(scene);
+            this.skyBg.setAmbient(1.0, 1.0, 1.0, 1.0);
+            this.skyBg.setEmission(1.0, 1.0, 1.0, 1.0);
+
+            this.backS = new CGFtexture(scene, "images/split_cubemap/back.png");
+            this.bottomS = new CGFtexture(scene, "images/split_cubemap/bottom.png");
+            this.frontS = new CGFtexture(scene, "images/split_cubemap/front.png");
+            this.leftS = new CGFtexture(scene, "images/split_cubemap/left.png");
+            this.rightS = new CGFtexture(scene, "images/split_cubemap/right.png");
+            this.topS = new CGFtexture(scene, "images/split_cubemap/top.png");
+
+            // ---
+
+            this.darkBg = new CGFappearance(scene);
+            this.darkBg.setAmbient(1.0, 1.0, 1.0, 1.0);
+            this.darkBg.setEmission(1.0, 1.0, 1.0, 1.0); 
+
+            this.backD = new CGFtexture(scene, "images/dark_cubemap/back.png");
+            this.bottomD = new CGFtexture(scene, "images/dark_cubemap/bottom.png");
+            this.frontD = new CGFtexture(scene, "images/dark_cubemap/front.png");
+            this.leftD = new CGFtexture(scene, "images/dark_cubemap/left.png");
+            this.rightD = new CGFtexture(scene, "images/dark_cubemap/right.png");
+            this.topD = new CGFtexture(scene, "images/dark_cubemap/top.png");
+        
+
+    }
+    display(selectedBackground) {
+        this.scene.pushMatrix();
+        this.scene.scale(50.0, 50.0, 50.0);
+
+        if(selectedBackground == 0) {
+            this.skyBg.setTexture(this.bottomS);
+            this.skyBg.apply();
+        }    
+        if (selectedBackground == 1) {
+            this.darkBg.setTexture(this.bottomD);
+            this.darkBg.apply();
+        }    
+        
+        this.scene.pushMatrix();
+        this.scene.rotate(Math.PI/2, 1, 0, 0);
+        this.scene.translate(0, 0, 0.5);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        if(selectedBackground == 0) {
+            this.skyBg.setTexture(this.frontS);
+            this.skyBg.apply();
+        }
+        if (selectedBackground == 1) {
+            this.darkBg.setTexture(this.frontD);
+            this.darkBg.apply();
+        }
+
+
+        this.scene.pushMatrix();
+        this.scene.translate(0, 0, 0.5); 
+        this.quad.display();
+        this.scene.popMatrix();
+
+        if(selectedBackground == 0) {
+            this.skyBg.setTexture(this.rightS);
+            this.skyBg.apply();
+        }    
+        if (selectedBackground == 1) {
+            this.darkBg.setTexture(this.rightD);
+            this.darkBg.apply();
+        } 
+
+        this.scene.pushMatrix();
+        this.scene.rotate(Math.PI/2, 0, 1, 0);
+        this.scene.translate(0, 0, 0.5);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        if(selectedBackground == 0) {
+            this.skyBg.setTexture(this.backS);
+            this.skyBg.apply();
+        }    
+        if (selectedBackground == 1) {
+            this.darkBg.setTexture(this.backD);
+            this.darkBg.apply();
+        } 
+
+        this.scene.pushMatrix();
+        this.scene.rotate(Math.PI, 0, 1, 0);
+        this.scene.translate(0, 0, 0.5);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        if(selectedBackground == 0) {
+            this.skyBg.setTexture(this.leftS);
+            this.skyBg.apply();
+        }    
+        if (selectedBackground == 1) {
+            this.darkBg.setTexture(this.leftD);
+            this.darkBg.apply();
+        } 
+
+        this.scene.pushMatrix();
+        this.scene.rotate(3 * Math.PI/2, 0, 1, 0);
+        this.scene.translate(0, 0, 0.5);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        if(selectedBackground == 0) {
+            this.skyBg.setTexture(this.topS);
+            this.skyBg.apply();
+        }    
+        if (selectedBackground == 1) {
+            this.darkBg.setTexture(this.topD);
+            this.darkBg.apply();
+        } 
+
+        this.scene.pushMatrix();
+        this.scene.rotate(-Math.PI/2, 1, 0, 0);
+        this.scene.translate(0, 0, 0.5);
+        this.quad.display();
+        this.scene.popMatrix();
+
+        this.scene.popMatrix();
+    }
+}
